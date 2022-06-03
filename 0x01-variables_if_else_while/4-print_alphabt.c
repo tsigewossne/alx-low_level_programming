5499cc956bb0e96d07c9d234a778b424425f70b5
tsigewossne <tsigedejen@gmail.com>
2022-06-03 20:52:17 +0300

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,16 @@
 
 int main(void)
 {
-char low;
+char low, e, q;
+
+e = 'e';
+q = 'q';
 
 for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
 putchar(low);
-for (low = 'A'; low <= 'Z'; low++)
-putchar(low);
+}
 putchar('\n');
-
 return (0);
 }
