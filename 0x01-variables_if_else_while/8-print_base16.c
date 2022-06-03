bf52483542384a743465430e76aac2a86ba836ac
tsigewossne <tsigedejen@gmail.com>
2022-06-03 21:06:36 +0300

    print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,11 @@
 
 int main(void)
 {
+int d;
 char low;
-
-for (low = 'z'; low >= 'a'; low--)
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+for (low = 'a'; low <= 'f'; low++)
 putchar(low);
 putchar('\n');
 
