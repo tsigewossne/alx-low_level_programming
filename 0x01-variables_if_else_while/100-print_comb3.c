73937731f3909a2e97b43fe8ce8a8094554b32a7
tsigewossne <tsigedejen@gmail.com>
2022-06-03 21:18:04 +0300

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,23 @@
 
 int main(void)
 {
-int d;
+int d, p;
 
-for (d = '0'; d <= '9'; d++)
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+if (p != d)
 {
 putchar(d);
-if (d != '9')
-{
+putchar(p);
+
+if (d == '8' && p == '9')
+continue;
+
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
