798277dbc7b4f1b690dca7098707bc27979b9477
tsigewossne <tsigedejen@gmail.com>
2022-06-10 21:53:02 +0300

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _print_diagonal - construct a diagonal based on given parameters
+ *@n : the number of times to print diagonal
+ *
+ * Return: empty
+ */
+void _print_diagonal(int n)
+{
+int x, y;
+if (n <= 0)
+{
+_putchar('\n')
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < x; y++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
