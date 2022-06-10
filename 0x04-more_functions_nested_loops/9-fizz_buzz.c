b4252653169e3124dc518318b753aef5a4e2dae7
tsigewossne <tsigedejen@gmail.com>
2022-06-10 22:42:21 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - to print the fizz-buzz number
+ * print_number_9 - to print the fizz-buzz number
  *
  *Description: print fizz for multiples of 3 and buzz for multiples of 5
  *
@@ -12,18 +12,22 @@ void print_number_9(void)
 int x;
 for (x = 0; x <= 100; x++)
 {
-if (x % 3 == 0)
+if (x % 15 == 0)
 {
-_putchar("fizz");
+printf("FizzBuzz");
+}
+else if (x % 3 == 0)
+{
+printf("fizz");
 }
 else if (x % 5 == 0)
 {
-_putchar("Buzz");
+printf("Buzz");
 }
 else
 {
-_putchar(x + '0');
+printf(x + '0');
 }
 }
-_putchar('\n');
+printf('\n');
 }
