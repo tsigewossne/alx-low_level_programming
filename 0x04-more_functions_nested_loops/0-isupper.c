8d142cf14b97fb5651bde9f1ca76d9aa967612fe
tsigewossne <tsigedejen@gmail.com>
2022-06-10 20:28:16 +0300

    return super

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - A function that checks for uppercase character.
+ * _isupper - A function that checks for uppercase character.
  *
  * @c: is the character to  check
  *
  * Return: 1 if c is uppercase otherwise 0
  */
-int main(int c)
+int _isupper(int c)
 {
-int a = isupper(c);
-
-if (a == 0)
-return (0);
-else
+if (c >= 65 && c <= 90)
+{
 return (1);
+}
+else
+{
+return (0);
+}
 }
