a7762ade773de64de1b5bdf221805866d9ee39f0
tsigewossne <tsigedejen@gmail.com>
2022-06-30 15:45:21 +0300

    main function

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-
 #endif
