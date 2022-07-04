0371ce479fde709de6276f6ec24062b8969af2cb
tsigewossne <tsigedejen@gmail.com>
2022-07-04 13:05:05 +0300

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - A simple struct that contains variables
- * @name: Holds name of dog in char array
- * @age: Holds number, including any decimals
- * @owner: Holds name of owner in char array
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+  char *name;
+  float age;
+  char *owner;
 };
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+#endif /* DOG_H */
