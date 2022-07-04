30ccf8874c1e20bfb7a77ffc7af77125d5dd1767
tsigewossne <tsigedejen@gmail.com>
2022-07-04 13:09:53 +0300

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,12 @@ float age;
 char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * dog - rename strucut with typedef
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
